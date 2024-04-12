87822bdfbdf541cebeb638a93143f95777f9689f
Gbeneti <gubeneti04@gmail.com>
2024-04-12 19:36:07 -0300

    added docstrings + minor corrections

--- ensemble_canonico_mistura.py
+++ ensemble_canonico_mistura.py
@@ -6,57 +6,48 @@ import seaborn as sns
 from tqdm import tqdm
 
 #constantes
-Kb = 8.6173324e-5
+Kb = 8.6173324e-5 #ev - cibstabte de boltzmann
 
-SIZE = 3
-#TEMPERATURAS = [i for i in range(15,600,40)]
-TEMPERATURAS = [30,40,50]
-TEMPERATURA = 250
-STEP = 1
+SIZE = 4 # Ordem da matriz
+TEMPERATURAS = [i for i in range(15,600,40)] # Range de temperaturas
+#TEMPERATURAS = [30,40,50]
 
-E_A = 0
-E_B = 0
-E_AA = -0.02
-E_BB = -0.02
-E_AB = -0.01
-N_A = 9
-N_B = (SIZE * SIZE) - N_A
-E_on_site = N_A * E_A + N_B * E_B
+E_A = 0 # Energia on site "A"
+E_B = 0 # Energia on site "B"
+E_AA = -0.02 # Energia interacao entre "A" e "A"
+E_BB = -0.02 # Energia interacao entre "B" e "B"
+E_AB = -0.01 # Energia interacao entre "A" e "B"
 
-N_A_range = np.array([n for n in np.arange(0,(SIZE*SIZE)+STEP,STEP)])
-N_B_range = N_A_range[::-1]
-N_A_percentage = N_A_range/(SIZE*SIZE)
+N_A_range = np.array([n for n in np.arange(0,(SIZE*SIZE)+1,1)]) # Range da quantidade de "A"
+N_B_range = N_A_range[::-1] # Range da quantidade de "B" considerando "A"
+N_A_percentage = N_A_range/(SIZE*SIZE) # Concentracao
 
 def possible_matrices(N_a, N_b):
     """
-    size: matrix order
-    N_a: number of 'A's to be placed
-    N_b: number of 'B's to be placed
+    N_a: numero de elementos "A"
+    N_b: numero de elementos "B"
     """
 
     global SIZE
     
-    # Check if the total number of elements matches the sum of N_a and N_b
+    # Check sum
     if N_a + N_b != SIZE * SIZE:
         raise ValueError("The total number of 'A's and 'B's should match the size of the matrix.")
     
-    # If N_a is zero, directly create a matrix filled with 'B's
     if N_a == 0:
         return np.full((1, SIZE, SIZE), 'B', dtype='<U1')
     
-    # Generate all possible combinations of indices for 'A's
+    # Gerar combinacoes
     indices_A = np.array(list(itertools.combinations(range(SIZE*SIZE), N_a)))
     
-    # Generate matrices with 'B's filled in all cells
+    # Gerar matrizes full "B"
     matrices = np.full((len(indices_A), SIZE, SIZE), 'B', dtype='<U1')
     
-    # Iterate through all possible combinations of indices for 'A's
+    # Indices possiveis "A"
     for i, indices in enumerate(indices_A):
-        # Fill 'A's at specified indices
         indices = np.unravel_index(indices, (SIZE, SIZE))
         matrices[i][indices] = 'A'
     
-    #print(len(matrices))
     return matrices
 
 def determinar_vizinhos(matriz):
@@ -65,7 +56,7 @@ def determinar_vizinhos(matriz):
     
     Args:
     
-        matriz: a célula da qual queremos saber os elementos e seus vizinhos
+        matriz: a célula da qual se quer saber os elementos e seus vizinhos
         
     Return:
     
@@ -90,25 +81,24 @@ def determinar_vizinhos(matriz):
 
     return vizinhos
 
-def energia_total(matriz):
-    """ Esta função calcula a energia de cada vizinho e a energia total da célula.
+def energia_total(matriz, N_a, N_b):
+    """ Esta função calcula a energia a energia total da celula.
     
     Args:
         matriz: configuração da célula.
-        E_AB: energias entre os elementos A e B.
-        E_AA: energias entre os elementos A e A.
-        E_BB: energias entre os elementos B e B.
-        E_on_site: energia para o próprio sítio.
+        N_a: numero de particulas "A"
+        N_b: numero de particulas "B"
         
     Return:
         energia_célula: energia total da configuração da célula.
-        energias_vizinhos: lista com os valores de energia entre cada vizinho.
     """
 
-    global E_AA, E_BB, E_AB, E_on_site
+    global E_A, E_B, E_AA, E_BB, E_AB
 
     vizinhos_dict = determinar_vizinhos(matriz)
     energias_vizinhos = np.array([])
+
+    E_on_site = N_a * E_A + N_b * E_B
 
     for coordenada, vizinhos in vizinhos_dict.items():
         i, j = coordenada
@@ -124,16 +114,16 @@ def energia_total(matriz):
     return energia_célula
 
 def helmholtz(energia_matrix_list, temperatura):
-    """Essa função calcula o valor da função de partição
+    """Essa função calcula o valor da energia de helmholtz com base na função de partição.
     
     Args:
         
-        energias: lista com a energia de cada configuração da célula.
-        temperatura: temperatura na qual estaria a célula.
+        energia_matrix_list: lista com a energia de cada configuração da celula.
+        temperatura: temperatura da celula.
         
     Return:
     
-        Z: valor da função de partição em uma temperatura.
+        F: energia de helmholtz da concentracao
     """
 
     global Kb
@@ -148,6 +138,17 @@ def helmholtz(energia_matrix_list, temperatura):
     return F
 
 def pappu(T, sigma):
+    """ Essa função calcula o campo medio.
+
+    Args:
+        T: Temperatura em K
+        sigma: Concentracao atual de A em relacao ao total.
+
+    Return:
+        F: Energia de Helmholtz dada a aproximacao de campo medio.
+
+    """
+
     global Kb, E_AA, E_AB, E_BB
 
     if (sigma == 0) or (sigma == 1):
@@ -163,25 +164,18 @@ def pappu(T, sigma):
 
     return F
 
-def determine_mean_field(temperaturas):
-    global N_A_percentage, SIZE
-
-    F_list = []
-
-    for T, percentage in tqdm(itertools.product(temperaturas, N_A_percentage),desc= "MF_temperature"):
-        F = pappu(T, percentage)
-        F_list.append(F)
-
-    temperaturas_list = [T]*((SIZE*SIZE) + 1)*len(temperaturas)
-    x_axis = list(N_A_range) * len(temperaturas)
-    concentration_ratio = list(N_A_percentage) * len(temperaturas)
-
-    data = pd.DataFrame({"Concentracao": concentration_ratio, "Energia de Helmholtz [eV]": F_list, "Temperatura [K]": temperaturas_list, "x_axis": x_axis})
-    return data
-
 def determinar_ensemble_canonico(temperaturas):
+    """ Essa funcao determina o DataFrame para o plot da energia de helmholtz em relacao a concentracao com variacao de temperatura, baseado no ensemble canonico.
 
-    global SIZE, N_A_range, N_B_range, N_A_percentage, E_AA, E_BB, E_AB, E_on_site
+    Args:
+        temperaturas: lista de temperaturas a serem utilizadas.
+
+    Return:
+        data: DataFrame contendo as concentracoes discretas, razao de concentracao,energia de hemholtz, temperaturas e a quantidade de particulas "A".
+
+    """
+
+    global SIZE, N_A_range, N_B_range, N_A_percentage, E_AA, E_BB, E_AB
 
     energia_matrix_list = []
     energia_helmholtz_list = []
@@ -191,9 +185,10 @@ def determinar_ensemble_canonico(temperaturas):
         for N_a, N_b in tqdm(zip(N_A_range,N_B_range), desc= "Concentration"):
             energia_matrix_list = []
             matrices = possible_matrices(N_a, N_b)
+            
 
-            for matrix in tqdm(matrices, desc="Permutations"):
-                energia_matrix = energia_total(matrix)
+            for matrix in tqdm(matrices, desc= "Permutation"):
+                energia_matrix = energia_total(matrix,N_a,N_b)
                 energia_matrix_list.append(energia_matrix)
         
             energia_helmholtz = helmholtz(energia_matrix_list,T)
@@ -208,6 +203,16 @@ def determinar_ensemble_canonico(temperaturas):
     return data
 
 def determinar_mean_field(temperaturas):
+    """ Essa funcao determina o DataFrame para o plot da energia de helmholtz em relacao a concentracao com variacao de temperatura, baseado na aproximacao de campo medio.
+
+    Args:
+        temperaturas: lista de temperaturas a serem utilizadas.
+
+    Return:
+        data: DataFrame contendo as concentracoes discretas, razao de concentracao,energia de hemholtz, temperaturas e a quantidade de particulas "A".
+
+    """
+
     global N_A_percentage, SIZE
 
     F_list = []
@@ -220,13 +225,25 @@ def determinar_mean_field(temperaturas):
     temperaturas_list = [T for T in temperaturas for _ in range(0,(SIZE*SIZE) + 1)]
     x_axis = list(N_A_range) * len(temperaturas)
     concentration_ratio = list(N_A_percentage) * len(temperaturas)
+    Na_Nb = [[i, (SIZE*SIZE) - i] for i in range((SIZE*SIZE) + 1)] * len(temperaturas)
 
-    data = pd.DataFrame({"Concentracao": concentration_ratio, "Energia de Helmholtz [eV]": F_list, "Temperatura [K]": temperaturas_list, "x_axis": x_axis})
+    data = pd.DataFrame({"[N_a, N_b]": Na_Nb,"Ratio": concentration_ratio, "Energia de Helmholtz [eV]": F_list, "Temperatura [K]": temperaturas_list, "x_axis": x_axis})
     return data
 
 def plot_helmholtz(data, img_name, color_pallete='tab10'):
+    """ Essa funcao plota algum dos dataframes.
+
+    Args:
+        data: DataFrame contendo os dados.
+        img_name: Nome em string da figura a ser salva. Deve conter o formato a ser salvo.
+        color_palette: palheta de cores usada para o plot.
+
+
+    Return:
+        data: DataFrame contendo as concentracoes discretas, razao de concentracao,energia de hemholtz, temperaturas e a quantidade de particulas "A".
+
+    """
     fig, ax = plt.subplots(figsize=(10, 6))
-    #sns.scatterplot(x=N_A_percentage, y=helmholtz_energies,c='red')
     sns.lineplot(x=data["x_axis"], y=data["Energia de Helmholtz [eV]"],hue=data["Temperatura [K]"],palette=color_pallete)
     plt.xlim(0, None)
     #plt.ylim(0, None)
